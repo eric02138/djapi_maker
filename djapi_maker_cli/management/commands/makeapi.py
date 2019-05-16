bc948d363e45eff2acd75db4b8fe5a85db91d7b5
eric02138 <emattison@gmail.com>
2019-05-16 17:34:36 -0400

    no changes to py command?

--- djapi_maker_cli/management/commands/makeapi.py
+++ djapi_maker_cli/management/commands/makeapi.py
@@ -63,14 +63,8 @@ class Command(BaseCommand):
     def make_app_config(self, db_name):
         self.apps_path = os.path.join(self.target_dir, "apps.py")
         with open(self.apps_path, 'w') as f:
-            apps_content = ""
-            apps_content += "from django.apps import AppConfig\n"
-            apps_content += "\n"
-            for class_name in self.class_names:
-                apps_content += "class {0}Config(AppConfig):\n".format(class_name)
-                apps_content += "    name = '{0}'\n".format(class_name.lower())
-                apps_content += "    label = '{0} label'\n".format(class_name)
-                apps_content += "\n"
+            apps_content = render_to_string('make_app_config.txt', {'db_name': db_name,
+                                                                    'class_names': self.class_names})
             f.write(apps_content)
             print(f"Created apps config file: {self.apps_path}")
 
@@ -78,95 +72,34 @@ class Command(BaseCommand):
     def make_serializers(self, db_name):
         self.serializers_path = os.path.join(self.target_dir, "serializers.py")
         with open(self.serializers_path, 'w') as f:
-            serializers_content = ""
-            serializers_content += "from rest_framework import serializers\n"
-            serializers_content += "\n"
-            for class_name in self.class_names:
-                serializers_content += "from {0}.models import {1}\n".format(db_name, class_name)
-            serializers_content += "\n"
-
-            for class_name in self.class_names:
-                serializers_content += "class {0}Serializer(serializers.ModelSerializer):\n".format(class_name)
-                serializers_content += "    class Meta:\n"
-                serializers_content += "        model = {0}\n".format(class_name)
-                serializers_content += "        fields = '__all__'\n"
-                serializers_content += "\n"
+            serializers_content = render_to_string('make_serializers.txt', {'db_name': db_name,
+                                                                            'class_names': self.class_names})
             f.write(serializers_content)
             print(f"Created serializers file: {self.serializers_path}")
 
     def make_views(self, db_name):
         self.views_path = os.path.join(self.target_dir, "views.py")
         with open(self.views_path, 'w') as f:
-            views_content = ""
-            views_content += "from rest_framework import generics\n"
-            views_content += "from rest_framework.views import APIView\n"
-            views_content += "from rest_framework.response import Response\n"
-            views_content += "from rest_framework.reverse import reverse\n"
-            for class_name in self.class_names:
-                views_content += "from {0}.serializers import {1}Serializer\n".format(db_name, class_name)
-                views_content += "from {0}.models import {1}\n".format(db_name, class_name)
-            views_content += "\n"
-            for class_name in self.class_names:
-                views_content += "class {0}List(generics.ListCreateAPIView):\n".format(class_name)
-                views_content += "    queryset = {0}.objects.all()\n".format(class_name)
-                views_content += "    serializer_class = {0}Serializer\n".format(class_name)
-                views_content += "\n"
-                views_content += "class {0}Detail(generics.RetrieveUpdateDestroyAPIView):\n".format(class_name)
-                views_content += "    queryset = {0}.objects.all()\n".format(class_name)
-                views_content += "    serializer_class = {0}Serializer\n".format(class_name)
-                views_content += "\n"
-            views_content += "class ModelList(APIView):\n"
-            views_content += "    def get(self, request, format=None):\n"
-            views_content += "        model_list = []\n"
-            for class_name in self.class_names:
-                views_content += "        model = {\n"
-                views_content += "            'model': '{0}',\n".format(class_name.lower())
-                views_content += "            'url': reverse('{0}_list_view', request=request)\n".format(class_name.lower())
-                views_content += "        }\n"
-                views_content += "        model_list.append(model)\n"
-            views_content += "        return Response(model_list)\n"
-            views_content += "\n"
+            views_content = render_to_string('make_views.txt', {'db_name': db_name,
+                                                                'class_names': self.class_names})
             f.write(views_content)
             print(f"Created views file: {self.views_path}")
 
     def make_urls(self, db_name):
         self.urls_path = os.path.join(self.target_dir, "urls.py")
         with open(self.urls_path, 'w') as f:
-            urls_content = ""
-            urls_content += "from django.urls import path\n"
-            urls_content += "from rest_framework.urlpatterns import format_suffix_patterns\n"
-            urls_content += "from {0} import views\n".format(db_name)
-            urls_content += "\n"
-            urls_content += "urlpatterns = [\n"
-            urls_content += "    path('', views.ModelList.as_view(), name='model_list_view'),\n"
-            for class_name in self.class_names:
-                urls_content += "    path('{0}', views.{1}List.as_view(), name='{0}_list_view'),\n".format(class_name.lower(), class_name)
-                urls_content += "    path('{0}/<int:pk>/', views.{1}Detail.as_view(), name='{0}_detail_view'),\n".format(class_name.lower(), class_name)
-            urls_content += "]\n"
-            urls_content += "\n"
-            urls_content += "urlpatterns = format_suffix_patterns(urlpatterns)\n"
-            urls_content += "\n"
+            urls_content = render_to_string('make_urls.txt', {'db_name': db_name,
+                                                              'class_names': self.class_names})
             f.write(urls_content)
             print(f"Created urls file: {self.urls_path}")
 
     def make_routers(self, db_name):
         self.router_path = os.path.join(self.target_dir, "router.py")
         with open(self.router_path, 'w') as f:
-            router_content = ""
-            for class_name in self.class_names:
-                router_content += "from {0}.models import {1}\n".format(db_name, class_name)
-            router_content += "\n"
-            router_content += "class Router(object):\n"
-            router_content += "\tdef db_for_read(self, model, **hints):\n"
-            router_content += "\t\tif model in [{0}]:\n".format(", ".join(self.class_names))
-            router_content += "\t\t\treturn '{0}'\n".format(db_name)
-            router_content += "\t\treturn None\n"
-            router_content += "\n"
-            router_content += "\tdef db_for_write(self, model, **hints):\n"
-            router_content += "\t\tif model in [{0}]:\n".format(", ".join(self.class_names))
-            router_content += "\t\t\treturn '{0}'\n".format(db_name)
-            router_content += "\t\treturn None\n"
-            router_content += "\n"
+            class_names_string = [ str(class_name) for class_name in class_names ]
+            router_content = render_to_string('make_routers.txt', {'db_name': db_name,
+                                                                   'class_names': self.class_names,
+                                                                   'class_names_string': class_names_string})
             f.write(router_content)
             print(f"Created db router file: {self.router_path}")
 
@@ -180,24 +113,7 @@ class Command(BaseCommand):
             os.utime(api_init, None)
         self.api_views_path = os.path.join(self.api_dir, "views.py")
         with open(self.api_views_path, 'w') as f:
-            views_content = ""
-            views_content += "from rest_framework import generics\n"
-            views_content += "from rest_framework.views import APIView\n"
-            views_content += "from rest_framework.response import Response\n"
-            views_content += "from rest_framework.reverse import reverse\n"
-            views_content += "\n"
-            views_content += "class AppList(APIView):\n"
-            views_content += "    def get(self, request, format=None):\n"
-            views_content += "        app_list = []\n"
-            for db_name in db_names:
-                views_content += "        app = {\n"
-                views_content += "            'app_name': '{0}',\n".format(db_name)
-                #views_content += "            'url': '/api/{0}'\n".format(db_name) #reverse url this when I'm not so tired
-                views_content += "            'url': reverse('{0}_link_view',  request=request)
-                views_content += "        }\n"
-                views_content += "        app_list.append(app)\n"
-            views_content += "        return Response(app_list)\n"
-            views_content += "\n"
+            views_content = render_to_string('make_api_views.txt', {'db_names': db_names})
             f.write(views_content)
             print(f"Created api views file: {self.api_views_path}")
 
@@ -212,18 +128,7 @@ class Command(BaseCommand):
             os.utime(api_init, None)
         self.api_urls_path = os.path.join(self.api_dir, "urls.py")
         with open(self.api_urls_path, 'w') as f:
-            urls_content = ""
-            urls_content += "from django.urls import path, include\n"
-            for db_name in db_names:
-                urls_content += "from {0} import urls as {0}_urls\n".format(db_name)
-            urls_content += "from api import views\n"
-            urls_content += "\n"
-            urls_content += "urlpatterns = [\n"
-            urls_content += "\tpath('', views.AppList.as_view(), name='app_list_view'),\n"
-            for db_name in db_names:
-                urls_content += "\tpath('{0}/', include({0}_urls)),\n".format(db_name)
-            urls_content += "]\n"
-            urls_content += "\n"
+            urls_content = render_to_string('make_api_urls.txt', {'db_names': db_names})
             f.write(urls_content)
             print(f"Created API urls file: {self.api_urls_path}")
 
